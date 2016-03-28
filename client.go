1c42c0a74dd495d2b01309d868f394f257fc5de9
Literate Snow <literatesnow@users.noreply.github.com>
2016-03-28 17:37:39 +1300

    Code format and add comments

--- client.go
+++ client.go
@@ -1,104 +1,99 @@
 package scgi
 
 import (
-  "io"
-  "net"
-  "fmt"
-  "bytes"
-  "strconv"
+	"bytes"
+	"io"
+	"net"
+	"strconv"
 )
 
 type Client struct {
-  headers map[string]string
-  conn net.Conn
+	network string
+	address string
+	headers map[string]string
 }
 
-func NewClient() *Client {
-  cl := &Client{ headers: make(map[string]string) }
+//NewClient creates a new SCGI client with default headers which will connect to a specified address.
+func NewClient(network string, address string) *Client {
+	cl := &Client{network: network, address: address}
 
-  cl.SetHeader("SCGI", "1")
-  cl.SetHeader("SERVER_PROTOCOL", "HTTP/1.1")
-  cl.SetHeader("REQUEST_METHOD", "POST")
+	//Default headers.
+	cl.headers = map[string]string{
+		"SCGI":           "1",
+		"REQUEST_METHOD": "POST"}
 
-  return cl
+	return cl
 }
 
-func (cl *Client) Connect(network string, address string) (err error) {
-  cl.conn, err = net.Dial(network, address)
-  if err != nil {
-    return err
-  }
+//Request sends a request to the server and reads the response.
+func (cl *Client) Request(request []byte) (response *bytes.Buffer, err error) {
+	conn, err := net.Dial(cl.network, cl.address)
+	if err != nil {
+		return nil, err
+	}
 
-  fmt.Printf("conn: %s\n", cl.conn);
+	err = cl.writeRequest(request, conn)
+	if err != nil {
+		return nil, err
+	}
 
-  return nil
+	response, err = cl.readResponse(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
 }
 
-func (cl *Client) Send(body string) string {
-  var bodyPart = []byte(body)
-  var headerPart = cl.requestHeader(len(bodyPart))
-  var buf = cl.netstring(headerPart, bodyPart)
+//writeRequest creates the underlying request and writes it to the socket.
+func (cl *Client) writeRequest(body []byte, conn net.Conn) (err error) {
+	header := cl.makeHeaders(len(body))
 
-  _, err := cl.conn.Write(buf)
-  if err != nil {
-    fmt.Printf("%s\n", err);
-    return ""
-  }
+	conn.Write(cl.netstring(header, body))
 
-  var fub bytes.Buffer
-  io.Copy(&fub, cl.conn)
-
-  fmt.Printf("%d: %s\n", fub.Len(), fub.Bytes());
-
-  return "nothing"
+	return nil
 }
 
-func (cl *Client) requestHeader(bodyLen int) []byte {
-  var headerPart = cl.appendHeader([]byte{}, "CONTENT_LENGTH", strconv.Itoa(bodyLen))
-
-  for k, v := range cl.headers {
-    headerPart = cl.appendHeader(headerPart, k, v)
-  }
-
-  return headerPart
+//readResponse reads the response from the server.
+func (cl *Client) readResponse(conn net.Conn) (response *bytes.Buffer, err error) {
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, conn)
+	return &buf, err
 }
 
+//makeHeaders creates the headers part of the request.
+func (cl *Client) makeHeaders(bodyLen int) []byte {
+	var headers = cl.appendHeader([]byte{}, "CONTENT_LENGTH", strconv.Itoa(bodyLen))
+
+	for k, v := range cl.headers {
+		headers = cl.appendHeader(headers, k, v)
+	}
+
+	return headers
+}
+
+//appendHeader appends a headers.
 func (cl *Client) appendHeader(buf []byte, key string, value string) []byte {
-  buf = append(buf, []byte(key) ...)
-  buf = append(buf, []byte{0} ...)
-  buf = append(buf, []byte(value) ...)
-  buf = append(buf, []byte{0} ...)
+	buf = append(buf, []byte(key)...)
+	buf = append(buf, []byte{0}...)
+	buf = append(buf, []byte(value)...)
+	buf = append(buf, []byte{0}...)
 
-  return buf
+	return buf
 }
 
-func (cl *Client) netstring(header []byte, body []byte) []byte {
-  buf := []byte(strconv.Itoa(len(header)))
-  buf = append(buf, ":" ...)
-  buf = append(buf, header ...)
-  buf = append(buf, "," ...)
-  buf = append(buf, body ...)
+//netstring creates a formatted netstring.
+func (cl *Client) netstring(headers []byte, body []byte) []byte {
+	buf := []byte(strconv.Itoa(len(headers)))
+	buf = append(buf, ":"...)
+	buf = append(buf, headers...)
+	buf = append(buf, ","...)
+	buf = append(buf, body...)
 
-  return buf
+	return buf
 }
 
+//SetHeader sets a header to a specific value
 func (cl *Client) SetHeader(name string, value string) {
-  cl.headers[name] = value
+	cl.headers[name] = value
 }
-
-func (cl *Client) Close() {
-  if cl.conn != nil {
-    cl.conn.Close()
-    cl.conn = nil
-  }
-}
-
-/*
-"70:"
-    "CONTENT_LENGTH" <00> "27" <00>
-    "SCGI" <00> "1" <00>
-    "REQUEST_METHOD" <00> "POST" <00>
-    "REQUEST_URI" <00> "/deepthought" <00>
-","
-"What is the answer to life?"
-*/
